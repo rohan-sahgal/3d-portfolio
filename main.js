c0210173c03f475f93e46027503e48f23a3a40d3
Rohan <rohan.sahgal@mail.utoronto.ca>
2021-05-23 11:35:33 -0400

    Set up scene and renderer (no objects currently)

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from "three";
+
+// Will always need 1. Scene 2. Camera 3. Renderer
+
+// Container that holds all objects, cameras, lights
+const scene = new THREE.Scene();
+
+// Perspective camera: mimics human eyes
+// (FOV, aspect ratio, view frustrum start, view frustrum end)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// Renderer renders elements of the scene
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Currently, camera is in middle of screen
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+// OBJECT CREATION
